af5ed360b4e0083f133b5662918ba3a6a8cd09a9
“linyan-z7” <1530100434@qq.com>
2026-05-11 14:24:42 +0800

    init

--- nook-server/src/main/java/com/nook/NookServerApplication.java
+++ nook-server/src/main/java/com/nook/NookServerApplication.java
@@ -2,6 +2,7 @@ package com.nook;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.scheduling.annotation.EnableScheduling;
 
 /**
  * Nook 启动入口。
@@ -9,6 +10,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * 扫描包覆盖全部 biz 模块（com.nook 下所有子包）。
  */
 @SpringBootApplication(scanBasePackages = "com.nook")
+@EnableScheduling
 public class NookServerApplication {
 
     public static void main(String[] args) {
